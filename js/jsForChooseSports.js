b873a95a13b1d16c4b651d8cab122a714fcd5454
Giorgio <34415646+GiorgioPeng@users.noreply.github.com>
2019-04-03 11:12:09 +0800

    Update jsForChooseSports.js

--- js/jsForChooseSports.js
+++ js/jsForChooseSports.js
@@ -11,3 +11,29 @@ sportChoose[0].addEventListener("touchstart",function(ev){
   }
 
 },false)
+//值选择
+sport_range = $("#sport_range");
+sport_range[0].addEventListener("change",function(){
+    switch (sport_range[0].value) {
+      case '0':
+        sport_range[0].style.backgroundSize="0% 100%";
+        break;
+      case '1':
+        sport_range[0].style.backgroundSize="20% 100%";
+        break;
+      case '2':
+        sport_range[0].style.backgroundSize="40% 100%";
+        break;
+      case '3':
+        sport_range[0].style.backgroundSize="60% 100%";
+        break;
+      case '4':
+        sport_range[0].style.backgroundSize="80% 100%";
+        break;
+      case '5':
+        sport_range[0].style.backgroundSize="100% 100%";
+        break;
+      default:
+
+    }
+},false)
