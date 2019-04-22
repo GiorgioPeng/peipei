8aceb6fa745634f2cd7d4668987ef9f064185f1f
Giorgio <34415646+GiorgioPeng@users.noreply.github.com>
2019-04-22 14:00:30 +0800

    Update JsForAirSmoking.js

--- js/JsForAirSmoking.js
+++ js/JsForAirSmoking.js
@@ -2,25 +2,25 @@ smoking_range = $("input[name='smoking_range']");
 smoking_range[0].addEventListener("change",function(){
   // console.log(typeof(smoking_range[0].value));
   // console.log(smoking_range[0].style.backgroundSize);
-  switch (smoking_range[0].value) {
+  switch (smoking_range.val()) {
     case '0':
       // console.log("here");
-      smoking_range[0].style.backgroundSize="0% 100%";
+      smoking_range.css("backgroundSize","0% 100%");
       break;
     case '1':
-      smoking_range[0].style.backgroundSize="20% 100%";
+      smoking_range.css("backgroundSize","20% 100%");
       break;
     case '2':
-      smoking_range[0].style.backgroundSize="40% 100%";
+      smoking_range.css("backgroundSize","40% 100%");
       break;
     case '3':
-      smoking_range[0].style.backgroundSize="60% 100%";
+      smoking_range.css("backgroundSize","60% 100%");
       break;
     case '4':
-      smoking_range[0].style.backgroundSize="80% 100%";
+      smoking_range.css("backgroundSize","80% 100%");
       break;
     case '5':
-      smoking_range[0].style.backgroundSize="100% 100%";
+      smoking_range.css("backgroundSize","100% 100%");
       break;
     default:
       // console.log("there");
@@ -30,17 +30,25 @@ smoking_range[0].addEventListener("change",function(){
  smoking_true = $('#smoking_true+label div');
  smoking_false = $('#smoking_false+label div');
  smoking_true[0].addEventListener('touchstart',function(){
-   if(smoking_true[0].style.transform!=="rotate(360deg)")
-   smoking_true[0].style.transform="rotate(360deg)";
+   if(smoking_true[0].style.transform!=="scale(1)")
+    {
+      smoking_true[0].style.transform="scale(1)";
+      smoking_true[0].style.transform="scale(2)";
+    }
    else {
-     smoking_true[0].style.transform="rotate(0deg)";
+    smoking_true[0].style.transform="scale(2)";
+    smoking_false[0].style.transform="scale(1)";
    }
 
  },false);
  smoking_false[0].addEventListener('touchstart',function(){
-   if(smoking_false[0].style.transform!=="rotate(360deg)")
-   smoking_false[0].style.transform="rotate(360deg)";
+   if(smoking_false[0].style.transform!=="scale(1)")
+    {
+      smoking_false[0].style.transform="scale(1)";
+      smoking_true[0].style.transform="scale(2)";
+    }
    else {
-     smoking_false[0].style.transform="rotate(0deg)";
+     smoking_false[0].style.transform="scale(2)";
+     smoking_true[0].style.transform="scale(1)";
    }
  },false);
