222c8821d13b90a31d4a7835068e2ed602755422
Giorgio <34415646+GiorgioPeng@users.noreply.github.com>
2019-04-22 13:59:08 +0800

    Update jsForInformation.js

--- js/jsForInformation.js
+++ js/jsForInformation.js
@@ -33,35 +33,84 @@ information_female[0].addEventListener("change",function(){
 //--------------------------------------------------------------------------------------
 information_button = $(".information_button");
 information_button[0].addEventListener("touchstart",function(){
-  $(".information")[0].style.display="none";
-  $(".sleep")[0].style.display="block"
+  $(".information").hide(500);
+  $(".sleep").show(500);
 },false);
 //------------------------------------------------------------------------------------------
 information_range = $("#information_range");
 information_range[0].addEventListener("change",function(){
   // console.log(typeof(information_range[0].value));
   // console.log(information_range[0].style.backgroundSize);
-  switch (information_range[0].value) {
+  switch (information_range.val()) {
     case '0':
       // console.log("here");
-      information_range[0].style.backgroundSize="0% 100%";
+      information_range.css("backgroundSize","0% 100%");
       break;
     case '1':
-      information_range[0].style.backgroundSize="20% 100%";
+      information_range.css("backgroundSize","20% 100%");
       break;
     case '2':
-      information_range[0].style.backgroundSize="40% 100%";
+      information_range.css("backgroundSize","40% 100%");
       break;
     case '3':
-      information_range[0].style.backgroundSize="60% 100%";
+      information_range.css("backgroundSize","60% 100%");
       break;
     case '4':
-      information_range[0].style.backgroundSize="80% 100%";
+      information_range.css("backgroundSize","80% 100%");
       break;
     case '5':
-      information_range[0].style.backgroundSize="100% 100%";
+      information_range.css("backgroundSize","100% 100%");
       break;
     default:
       // console.log("there");
   }
 },false);
+
+//______________________________________________________________________专业选择下拉框
+let major_choose_detail = $(".major_choose_detail");
+let major_choose = $(".major_choose");
+let major = $("#major_choose");
+let major_text = $("#major_choose p")
+major[0].addEventListener("touchstart",function(){
+    major_choose.slideToggle();
+},false);
+for (let i = 0; i < major_choose_detail.length; i++) {
+  major_choose_detail[i].addEventListener('touchstart',function(){
+      major_text.text(major_choose_detail[i].innerHTML);
+  },false)
+}
+//________________________________________________________________________________
+let n_o_s = $("#n_o_s");
+let area_choose = $("#area_choose");
+let area_choose_detail = $(".area_choose_detail");
+let n_o_s_text = $("#n_o_s p")
+n_o_s[0].addEventListener("touchstart",function(){
+  area_choose.slideToggle();
+},false);
+for(let i = 0 ; i < area_choose_detail.length;i++){
+  area_choose_detail[i].addEventListener("touchstart",function(){
+    n_o_s_text.text(area_choose_detail[i].innerHTML);
+  },false);
+}
+
+
+let provence = $("#provence");
+let s_provence = $("#s_provence");
+let n_provence = $("#n_provence");
+let p_choose = $(".p_choose");
+let provence_text = $("#provence p");
+provence[0].addEventListener("touchstart",function(){
+  if(n_o_s_text.text()=="南方")
+  {
+    s_provence.slideToggle();
+  }
+  if(n_o_s_text.text()=="北方")
+  {
+    n_provence.slideToggle();
+  }
+},false);
+for(let i = 0 ; i < p_choose.length;i++){
+  p_choose[i].addEventListener("touchstart",function(){
+    provence_text.text(p_choose[i].innerHTML);
+  },false);
+}
